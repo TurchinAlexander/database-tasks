d35dba89741a39391b6438c3a79d0e8e2afdef68
Alexander <AlexanderTurchin1@gmail.com>
2019-09-24 23:09:17 +0300

    Lab 6 is done

---
+++ Lab6/Lab6.sql
@@ -0,0 +1,54 @@
+use AdventureWorks2012
+go
+
+if object_id('dbo.SubCategoriesByColor') is not null
+begin
+	drop procedure dbo.SubCategoriesByColor
+end
+go
+
+create procedure dbo.SubCategoriesByColor (
+	@colors nvarchar(max)
+)
+as
+declare
+	@sql nvarchar(max) = ''
+	,@pivot_headers nvarchar(max) = ''
+	,@pivot_rows nvarchar(max) = ''
+begin
+	select 
+		@pivot_headers += ',[' + value + '] as ' + value,
+		@pivot_rows += '[' + value + '],'
+	from
+		string_split(@colors, ',')
+		
+	set	@pivot_rows = LEFT(@pivot_rows, LEN(@pivot_rows) - 1)
+
+	set @sql = '
+	select
+		SubcategoryName as Name' + @pivot_headers + '
+	from (
+		select
+			p.Color
+			,p.[Weight]
+			,ps.Name as SubcategoryName
+		from
+			[Production].[Product] p
+		inner join
+			[Production].[ProductSubcategory] ps
+				on
+					ps.ProductSubcategoryID = p.ProductSubcategoryID
+	) as SourceTable
+	pivot(
+		max([Weight])
+		for Color in (' + @pivot_rows + ')
+	
+	) as PivotTable
+'
+	print @sql
+
+	exec sp_executesql @sql
+end
+go
+
+EXECUTE dbo.SubCategoriesByColor 'Black,Silver,Yellow'
