4ccc8fed63abdc74f3b5377d268c9675aa88f73b
Alexander <AlexanderTurchin1@gmail.com>
2019-09-24 23:09:25 +0300

    Lab 7 is done.

---
+++ Lab7/Lab7.sql
@@ -0,0 +1,32 @@
+use AdventureWorks2012
+go
+
+create procedure dbo.ParseXML(
+	@xml xml
+)
+as
+begin
+
+	select
+		n.value('@ID', 'int') as ProductID,
+		n.value('(./Name)[1]', 'varchar(50)') as ProductName
+		,n.value('(./ProductNumber)[1]', 'varchar(50)') as ProductNumber
+	from
+		@xml.nodes('/Products/Product') as T(n)
+end
+go
+
+declare @xml xml
+
+set @xml = (
+	select
+		p.ProductID as '@ID'
+		,p.Name
+		,p.ProductNumber
+	from
+		[Production].[Product] p
+	for
+		xml path('Product'), root('Products')
+)
+
+exec dbo.ParseXML @xml
